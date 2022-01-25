961e8ff8c68448f3abad6d0dcbc7b150736f9b68
Anjan Prasad <anjanprasad112@gmail.com>
2022-01-25 14:39:49 +0530

    init

---
+++ textutils/app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index. <h1><a href='/home'>forword</a></h1>")
+def home(request):
+    return HttpResponse('''<h1>hello, <a href="https://docs.djangoproject.com/en/4.0/intro/tutorial01/">link django docs
+    </a> <a href='/'>back</a> </h1>''')
+def login(request):
+    params={'name':'Anjan prasad','job':'Web developer'}
+    return render(request, 'index.html',params)
+def removepunc(request):
+    maths =int(request.GET.get('maths','default'))
+    science =int( request.GET.get('science','default'))
+    social =int( request.GET.get('social','default'))
+    total = maths+science+social
+    percent=float(total/3)
+    print(percent)
+    json = {'total':total,'percentage':percent}
+    return render(request,'percent.html',json)
+def form(request):
+    return render(request, 'form.html')
